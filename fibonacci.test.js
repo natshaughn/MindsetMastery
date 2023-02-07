4e3fd42877ae05995ced744be817ce0a0f9e28c7
Nathan Shaughnessy <nathan.shaughnessy@roq.local>
2023-02-07 15:59:16 +0000

    second push with some more tests

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -26,8 +26,9 @@ test('When skip is initialized it should move forward and return the number furt
 
 test('When a high number initialized, the right next number should be returned', () => {
     fibonacci.init(2584);
-    expect(fibonacci.next())
-})
+    expect(fibonacci.next()).toBe(4181);
+});
+
 
 // Negative tests
 // Jest Example 
@@ -37,9 +38,21 @@ test('When a high number initialized, the right next number should be returned',
 //     }).toThrow();
 //   });
 
-test('throws on 7', () => {
+test('throws on 7 as it is not a fibby num', () => {
     expect(() => {
         fibonacci.init(7);
+    }).toThrow();
+});
+
+test('throws on -1 skip as it is a go forward function', () => {
+    expect(() => {
+        fibonacci.skip(-1);
+    }).toThrow();
+});
+
+test('throws on "one" as it is a string', () => {
+    expect(() => {
+        fibonacci.init('one');
     }).toThrow();
 });
 
